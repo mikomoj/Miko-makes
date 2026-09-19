6abbed08f7d286a77ffc516f5f473edc960ae692
MIKOMOJ <ankursahutala27@gmail.com>
2026-09-19 22:57:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,14 +30,14 @@ no.addEventListener("click", () => {
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
-    gif.src = "D:/miko-start/miko-start-N/miko-start-web/resources/3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
+    gif.src = "3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
     text.innerHTML = "Your hand must have slipped right?🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
-    gif.src = "D:/miko-start/miko-start-N/miko-start-web/resources/4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
+    gif.src = "4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
     text.innerHTML = "I'm gonna cry😭";
     yes.style.height = "80%";
     yes.style.width = "80%";
@@ -45,7 +45,7 @@ no.addEventListener("click", () => {
     no.style.width = "10%";
     count++;
   } else if (count == 5) {
-    gif.src = "D:/miko-start/miko-start-N/miko-start-web/resources/4.crying.gif";// gif credit: https://tenor.com/view/cat-gif-10173437195524493032
+    gif.src = "5.crying.gif";// gif credit: https://tenor.com/view/cat-gif-10173437195524493032
     text.innerHTML = "Pretty Please🥺😘";
     yes.style.height = "90%";
     yes.style.width = "96%";
@@ -56,7 +56,7 @@ no.addEventListener("click", () => {
 // changes for when YES button is clicked
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "D:/miko-start/miko-start-N/miko-start-web/resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
+  gif.src = "idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
   text.innerHTML = "Knew it babe 😘";
   yes.innerHTML = '<a href="https://wa.me/qr/VNC2O73PV75NJ1"> Message me </a>';// inside the " " put your social media profile link
   yes.style.height = "90%";
